b3de79150f9374b385f744e01fcc5d9aba0c97b6
DanilaNova <4321danila@gmail.com>
2022-07-05 21:45:28 +0300

    Fixing installer

--- Installer.lua
+++ Installer.lua
@@ -1,21 +1,21 @@
 print("Launching installer...")
+local filesystem = require("filesystem")
+local computer = require("computer")
+local dict = {}
 
 print("Select language. Выберите язык. (Eng/Рус)")
-for(true) do
+while true do
   sel = io.read()
-  if(sel:lower() = "eng") then 
-    local dict = {"Prepairing working folder...","Downloading files...","Done. Do you want to reboot?(Y/n)","Reinstalling the terminal..."}
+  if(string.lower(sel) == "eng") then 
+    dict = {"Prepairing working folder...","Downloading files...","Done. Do you want to reboot?(Y/n)","Reinstalling the terminal..."}
     break
-  elseif(sel:lower() = "рус")
-    local dict = {"Подготовка рабочей папки...","Загрузка файлов...","Готово. Хотите перезагрузить?(Y/n)","Переустановка терминала..."}
+  elseif(string.lower(sel) == "рус") then
+    dict = {"Подготовка рабочей папки...","Загрузка файлов...","Готово. Хотите перезагрузить?(Y/n)","Переустановка терминала..."}
     break
   end
 end
 
-local filesystem = require("filesystem")
-local computer = require("computer")
-
-if(not (filesystem.exists("/home/MineTerm"))) then
+if(not filesystem.exists("/home/MineTerm")) then
   print(dict[1])
   filesystem.makeDirectory("/home/MineTerm")
   filesystem.makeDirectory("/home/MineTerm/logs")
@@ -25,15 +25,15 @@ if(not (filesystem.exists("/home/MineTerm"))) then
   print(dict[2])
   os.execute("wget 'https://pastebin.com/raw/UkqQNak7' /home/MineTerm/Mineterm")
   print(dict[3])
-  for(true) do
+  while true do
     sel = io.read()
-    if(sel:lower() = "y") then 
+    if(sel:lower() == "y") then 
       computer.shutdown(true)
-    elseif(sel:lower() = "n") then
+    elseif(sel:lower() == "n") then
       break
     end
   end
-elseif(not (filesystem.exists("home/MineTerm/logs"))) then
+elseif(not filesystem.exists("home/MineTerm/logs")) then
   print(dict[4])
   filesystem.makeDirectory("/home/MineTerm/logs")
   os.execute("wget 'https://pastebin.com/raw/UkqQNak7' /home/MineTerm/Mineterm")
